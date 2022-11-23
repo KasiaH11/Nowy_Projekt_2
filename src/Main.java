41cf5a1c0772342195ab128f000e211179948c29
KasiaH11 <katarzyna.haladyniak.praca@onet.pl>
2022-11-23 15:00:05 +0100

    zwracanie reszty

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Podaj pierwsza cyfre");
-        Scanner scanner=new Scanner(System.in);
-        int number1=scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
         System.out.println("Podaj druga cyfre");
-        int number2=scanner.nextInt();
-        System.out.println("Podaj trzecia cyfre");
-        int number3=scanner.nextInt();
-        System.out.println("Suma" +(number1+number2+number3));
+        int number2 = scanner.nextInt();
+        System.out.println("Suma" + (number1 % +number2));
+        if (number1 > number2) {
+            System.out.println(number1+"jest wieksza od "+number2);
+        }
     }
 }
+

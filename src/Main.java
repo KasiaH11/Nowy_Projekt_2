e6e693a8062e8501750b11b1fcef66ddd499c3a9
KasiaH11 <katarzyna.haladyniak.praca@onet.pl>
2022-11-23 14:50:16 +0100

    Przedstawienie sie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name="Kasia";
+        int age=18;
+        System.out.println("Czesc mam na imie "+name+" mam " +age+" lat");
     }
 }

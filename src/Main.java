8e93baa5efc12c26b8dd6adbf169bda8f38ef7cf
KasiaH11 <katarzyna.haladyniak.praca@onet.pl>
2022-11-23 14:58:40 +0100

    suma

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String name="Kasia";
-        int age=18;
-        System.out.println("Czesc mam na imie "+name+" mam " +age+" lat");
+        System.out.println("Podaj pierwsza cyfre");
+        Scanner scanner=new Scanner(System.in);
+        int number1=scanner.nextInt();
+        System.out.println("Podaj druga cyfre");
+        int number2=scanner.nextInt();
+        System.out.println("Podaj trzecia cyfre");
+        int number3=scanner.nextInt();
+        System.out.println("Suma" +(number1+number2+number3));
     }
 }
